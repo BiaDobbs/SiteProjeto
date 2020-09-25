29fd2909a00055446ee923c77add8043daaf007e
BiaDobbs <71426029+BiaDobbs@users.noreply.github.com>
2020-09-25 19:25:52 -0300

    Add files via upload

---
+++ Home/Home.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(largura, altura);
+  atividade1= createButton("atividade1");
+  atividade2= createButton("atividade2");
+  atividade3= createButton("atividade3");
+}
+
+function draw() {
+
+}
