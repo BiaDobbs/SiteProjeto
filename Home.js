20281bd7ae27ef800f51aa20687cc7ff51024d4b
BiaDobbs <beatrizdobbs10@gmail.com>
2020-10-08 22:47:47 -0300

    4

--- Home.js
+++ Home.js
@@ -1,8 +1,8 @@
 function setup() {
-  createCanvas(largura, altura);
-  atividade1= createButton("atividade1");
-  atividade2= createButton("atividade2");
-  atividade3= createButton("atividade3");
+    createCanvas(largura, altura);
+    atividade1 = createButton("atividade1");
+    atividade2 = createButton("atividade2");
+    atividade3 = createButton("atividade3");
 }
 
 function draw() {
