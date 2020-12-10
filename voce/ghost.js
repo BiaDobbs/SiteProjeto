a0a22dea9699a2a17f149cb0450e8d4ea7447e53
BiaDobbs <71426029+BiaDobbs@users.noreply.github.com>
2020-12-10 06:16:24 -0300

    sei lá mano

--- voce/ghost.js
+++ voce/ghost.js
@@ -1,20 +1,20 @@
 function ghost() {
 
-  var numeros = [127, 200, 100, 225, 170, 135, 250, 230, 187, 153];
+    var numeros = [127, 200, 100, 225, 170, 135, 250, 230, 187, 153];
 
-  image(video, 0, 0, width, height);
-  tint(255, 100);
-  filter(INVERT);
-  image(video, 0, 0, width, height);
-  tint(255, transp);
+    image(video, 0, 0, width, height);
+    tint(255, 100);
+    filter(INVERT);
+    image(video, 0, 0, width, height);
+    tint(255, transp);
 
-  muda++;
-  
-  if (muda == 10) {
-    //transp = round(random(50, 250));
-    transp = random(numeros);
-    muda = 0;
-  }
-  //print(muda);
-  
+    muda++;
+
+    if (muda == 10) {
+        transp = round(random(50, 250));
+        transp = random(numeros);
+        muda = 0;
+    }
+    //print(muda);
+
 }
--- voce/nostalgia.js
+++ voce/nostalgia.js
@@ -1,8 +1,5 @@
 function nostalgia() {
   fill(255);
-  //noStroke();
-  //triangle(30, 75, 58, 20, 86, 75);
-  //triangle(100, 200, 100*2, 200*2, 100/2, 200/2);
   textSize(50);
   video.loadPixels();
   var stepSize = 7;
@@ -15,23 +12,17 @@ function nostalgia() {
 
       if (redVal<90) {
         //stroke(redVal-30, greenVal, blueVal*2);
-        //stroke(0);
         stroke(15, 19, 25);
       } else if (redVal>90 && redVal<150) {
         //stroke(redVal/2, greenVal/2, blueVal);
-        //stroke(255);
         stroke(80, 10, 204);
       } else if (redVal>150 && redVal<200) {
         //stroke(redVal/2, greenVal/2, blueVal);
-        //stroke(255);
         stroke(80, 100, 204);
       } else if (redVal>200) {
         stroke(110, 102, 255);
         //stroke(redVal-30, greenVal, blueVal*2);
       } 
-      //stroke(redVal/2, greenVal+10, blueVal);
-      //let letra = random(texto);
-      //text(letra, x, y, width, height);
       strokeWeight(stepSize);
       triangle(x, y, x+stepSize, y+stepSize, x-stepSize, y-stepSize);
       //ellipse(x, y, stepSize, stepSize);
